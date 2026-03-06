932fc6aa6a2620b8890268ac11ab3628c66c8080
Stephan Meijer <me@stephanmeijer.com>
2026-03-06 16:46:13 +0100

    migrate i3 config to Home Manager with wezterm as default terminal

--- workstation.nix
+++ workstation.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+
+  pkgs,
+  ...
+}:
 
 let
   mod = config.xsession.windowManager.i3.config.modifier;
@@ -19,11 +24,151 @@ in
   # i3 window manager
   xsession.windowManager.i3 = {
     enable = true;
+
     config = {
       modifier = "Mod4";
-      keybindings = lib.mkOptionDefault {
-        "${mod}+c" = "exec --no-startup-id clipmenu";
+
+      fonts = {
+        names = [ "Ubuntu Mono" ];
+        size = 12.0;
       };
+
+      terminal = "${pkgs.wezterm}/bin/wezterm";
+
+      startup = [
+        {
+          command = "dex --autostart --environment i3";
+          notification = false;
+        }
+        {
+          command = "xss-lock --transfer-sleep-lock -- i3lock --nofork";
+          notification = false;
+        }
+        {
+          command = "nm-applet";
+          notification = false;
+        }
+      ];
+
+      keybindings = {
+        # Terminal
+        "${mod}+Return" = "exec wezterm";
+
+        # Kill
+        "${mod}+Shift+q" = "kill";
+
+        # Rofi
+        "${mod}+space" = ''exec "rofi -modi drun,run,window -show drun -theme material -font 'mono 24'"'';
+        "${mod}+Tab" = ''exec "rofi -modi drun,run,window -show window -theme material -font 'mono 24'"'';
+
+        # Clipmenu
+        "${mod}+c" = "exec --no-startup-id clipmenu";
+
+        # Volume (pactl + i3blocks refresh)
+        "XF86AudioRaiseVolume" =
+          "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && killall -SIGUSR1 i3blocks";
+        "XF86AudioLowerVolume" =
+          "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && killall -SIGUSR1 i3blocks";
+        "XF86AudioMute" =
+          "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && killall -SIGUSR1 i3blocks";
+        "XF86AudioMicMute" =
+          "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && killall -SIGUSR1 i3blocks";
+
+        # Focus
+        "${mod}+j" = "focus left";
+        "${mod}+k" = "focus down";
+        "${mod}+l" = "focus up";
+        "${mod}+semicolon" = "focus right";
+        "${mod}+Left" = "focus left";
+        "${mod}+Down" = "focus down";
+        "${mod}+Up" = "focus up";
+        "${mod}+Right" = "focus right";
+
+        # Move
+        "${mod}+Shift+j" = "move left";
+        "${mod}+Shift+k" = "move down";
+        "${mod}+Shift+l" = "move up";
+        "${mod}+Shift+semicolon" = "move right";
+        "${mod}+Shift+Left" = "move left";
+        "${mod}+Shift+Down" = "move down";
+        "${mod}+Shift+Up" = "move up";
+        "${mod}+Shift+Right" = "move right";
+
+        # Split
+        "${mod}+h" = "split h";
+        "${mod}+v" = "split v";
+
+        # Fullscreen
+        "${mod}+f" = "fullscreen toggle";
+
+        # Layout
+        "${mod}+s" = "layout stacking";
+        "${mod}+w" = "layout tabbed";
+        "${mod}+e" = "layout toggle split";
+
+        # Float
+        "${mod}+Shift+space" = "floating toggle";
+
+        # Focus parent
+        "${mod}+a" = "focus parent";
+
+        # Workspaces
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
+        "${mod}+3" = "workspace number 3";
+        "${mod}+4" = "workspace number 4";
+        "${mod}+5" = "workspace number 5";
+        "${mod}+6" = "workspace number 6";
+        "${mod}+7" = "workspace number 7";
+        "${mod}+8" = "workspace number 8";
+        "${mod}+9" = "workspace number 9";
+        "${mod}+0" = "workspace number 10";
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+        "${mod}+Shift+6" = "move container to workspace number 6";
+        "${mod}+Shift+7" = "move container to workspace number 7";
+        "${mod}+Shift+8" = "move container to workspace number 8";
+        "${mod}+Shift+9" = "move container to workspace number 9";
+        "${mod}+Shift+0" = "move container to workspace number 10";
+
+        # Session
+        "${mod}+Shift+c" = "reload";
+        "${mod}+Shift+r" = "restart";
+        "${mod}+Shift+e" =
+          ''exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'"'';
+
+        # Resize mode
+        "${mod}+r" = "mode resize";
+      };
+
+      modes = {
+        resize = {
+          "j" = "resize shrink width 10 px or 10 ppt";
+          "k" = "resize grow height 10 px or 10 ppt";
+          "l" = "resize shrink height 10 px or 10 ppt";
+          "semicolon" = "resize grow width 10 px or 10 ppt";
+          "Left" = "resize shrink width 10 px or 10 ppt";
+          "Down" = "resize grow height 10 px or 10 ppt";
+          "Up" = "resize shrink height 10 px or 10 ppt";
+          "Right" = "resize grow width 10 px or 10 ppt";
+          "Return" = "mode default";
+          "Escape" = "mode default";
+          "${mod}+r" = "mode default";
+        };
+      };
+
+      bars = [
+        {
+          statusCommand = ''SCRIPT_DIR="/home/steve/.config/i3blocks/scripts" i3blocks'';
+        }
+      ];
     };
+
+    extraConfig = ''
+      tiling_drag modifier titlebar
+    '';
   };
 }
